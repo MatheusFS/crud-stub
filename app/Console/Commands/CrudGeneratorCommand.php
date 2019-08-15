5655dddb8034cd5a76122f94a6c461a44a0b90eb
MatheusFS <matheusfs97@gmail.com>
2019-08-15 15:03:20 -0300

    First commit

---
+++ app/Console/Commands/CrudGeneratorCommand.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class CrudGeneratorCommand extends Command {
+
+    protected $signature = 'crud:generate {name : Class (singular) for example User}';
+
+    protected $description = 'Create CRUD operations';
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function handle() {
+        $name = $this->argument('name');
+
+        $this->controller($name);
+        $this->model($name);
+        $this->views($name);
+
+        $resourceRouteLine = "Route::resource('".str_plural(strtolower($name))."', '".$name."Controller')->middleware('auth');";
+        File::append(base_path('routes/web.php'), $resourceRouteLine);
+    }
+
+    protected function getStub($type) {
+
+        return file_get_contents(resource_path("stubs/$type.stub"));
+    }
+
+    protected function model($name) {
+
+        $modelTemplate = str_replace(
+            ['{{modelName}}'],
+            [$name],
+            $this->getStub('Model')
+        );
+
+        file_put_contents(app_path("/{$name}.php"), $modelTemplate);
+    }
+
+    protected function controller($name) {
+
+        $controllerTemplate = str_replace(
+            [
+                '{{modelName}}',
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}'
+            ],
+            [
+                $name,
+                strtolower(str_plural($name)),
+                strtolower($name)
+            ],
+            $this->getStub('Controller')
+        );
+
+        file_put_contents(app_path("/Http/Controllers/{$name}Controller.php"), $controllerTemplate);
+    }
+
+    protected function request($name) {
+
+        $requestTemplate = str_replace(
+            ['{{modelName}}'],
+            [$name],
+            $this->getStub('Request')
+        );
+
+        if (!file_exists($path = app_path('/Http/Requests')))
+            mkdir($path, 0777, true);
+
+        file_put_contents(app_path("/Http/Requests/{$name}Request.php"), $requestTemplate);
+    }
+
+    protected function views($name) {
+
+        $viewIndexTemplate = str_replace(
+            [
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}'
+            ],
+            [
+                strtolower(str_plural($name)),
+                strtolower($name)
+            ],
+            $this->getStub('ViewIndex')
+        );
+
+        $viewShowTemplate = str_replace(
+            [
+                '{{modelNamePluralLowerCase}}',
+                '{{modelNameSingularLowerCase}}'
+            ],
+            [
+                strtolower(str_plural($name)),
+                strtolower($name)
+            ],
+            $this->getStub('ViewShow')
+        );
+
+
+        if (!is_dir(resource_path("views/$name")))
+            mkdir(resource_path("views/$name"));
+
+        file_put_contents(resource_path("views/$name/index.blade.php"), $viewIndexTemplate);
+        file_put_contents(resource_path("views/$name/show.blade.php"), $viewShowTemplate);
+    }
+}
